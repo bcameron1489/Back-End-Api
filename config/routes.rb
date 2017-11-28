21ebe3f4cea87a5fb314cd4f9e126c7cfb02c60b
bcameron1489 <bcameron1489@gmail.com>
2017-11-28 13:58:48 -0500

    Setup

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: %i[index show]
+
+  get '/players' => 'players#index'
+  get '/players/:id' => 'players#show'
+  delete '/players/:id' => 'players#destroy'
+  patch '/players/:id' => 'players#update'
+  post '/players' => 'players#create'
 end
