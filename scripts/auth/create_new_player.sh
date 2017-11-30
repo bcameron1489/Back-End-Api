0f2c587a3c273d3c32725b502b6c13289c2a6ff1
bcameron1489 <bcameron1489@gmail.com>
2017-11-30 11:17:03 -0500

    Add working player curl
    Curl script for player working

--- scripts/auth/create_new_player.sh
+++ scripts/auth/create_new_player.sh
@@ -4,13 +4,14 @@ curl http://localhost:4741/players \
   --header "Authorization: Token token=${TOKEN}" \
   --header "Content-Type: application/json" \
   --data '{
-    "player":
-      "name": "'"${PLAYER}"'",
+    "player": {
+      "name": "'"${NAME}"'",
       "team": "'"${TEAM}"'",
       "bye": "'"${BYE}"'"
     }
   }'
 
+# PLAYER='Bobson Duggnut' TEAM=NYE BYE=1 TOKEN="BAhJIiVmZGY3NTQ3YmY5Nzc2NWEzMmE0NjYzY2IwM2QwZTk2NgY6BkVG--0c823a8830c765c955110671175c7a70c51f4375" sh
 
   # create_table "players", force: :cascade do |t|
   #   t.string "name"
