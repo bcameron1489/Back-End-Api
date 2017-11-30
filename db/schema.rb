e41d725410c3f3f746ec6b07dd9281539023bd3f
bcameron1489 <bcameron1489@gmail.com>
2017-11-30 10:56:06 -0500

    Remove user id from player column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130152444) do
+ActiveRecord::Schema.define(version: 20171130155431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,6 @@ ActiveRecord::Schema.define(version: 20171130152444) do
     t.integer "bye"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "users_id"
-    t.index ["users_id"], name: "index_players_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,5 +42,4 @@ ActiveRecord::Schema.define(version: 20171130152444) do
   end
 
   add_foreign_key "examples", "users"
-  add_foreign_key "players", "users", column: "users_id"
 end
