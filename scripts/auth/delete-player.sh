8d15744163d337bc3ad067c311beb5515fb5555c
bcameron1489 <bcameron1489@gmail.com>
2017-11-30 22:01:36 -0500

    Complete working crud (delete players may need tweaking)
    Add all working crud and auth actions

--- scripts/auth/delete-player.sh
+++ scripts/auth/delete-player.sh
@@ -1,6 +1,7 @@
 API="${API_ORIGIN:-http://localhost:4741}"
-URL_PATH="/players"
+URL_PATH="/fantasy_players"
 curl "${API}${URL_PATH}/${ID}" \
   --include \
   --request DELETE \
+  --header "Authorization: Token token=${TOKEN}" \
   --header "Content-Type: application/json" \
